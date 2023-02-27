89205a872d267fd33feb8786bac9026e8d717e87
student203 <student203@203-03>
2023-02-27 11:04:17 +0500

    Добавьте файлы проекта.

---
+++ работа 1/Form1.cs
@@ -0,0 +1,20 @@
+namespace работа_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "=4";
+        }
+    }
+}
